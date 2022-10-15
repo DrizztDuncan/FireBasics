71367062e384440d4a792ce2c0c6b2ffa72b4d1f
Duncan <duncantheinvictus@gmail.com>
2022-10-15 15:38:05 +0800

    firebase auth

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
+//firebase states
 console.log(firebase);
+
+//firebase auth
+const auth = firebase.auth();
+//Grab HTML elements
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const userDetails = document.getElementById("userDetails");
+//Google Auth popup
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
